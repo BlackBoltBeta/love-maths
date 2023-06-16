0cde6e191c2cb59fc84eac7d310b45ea90e1681e
Nicolas Saavedra <h.n.saavedra@gmail.com>
2023-06-16 14:36:52 +0000

    feat(js):add structure of game

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWongAnswer() {
+
+}
+
+function displayAditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
